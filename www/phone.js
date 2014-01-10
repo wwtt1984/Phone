13f0744f4096df881c977e94501034e69435b1c2
master <wwtt1984@163.com>
2014-01-10 09:37:17 +0800

    first app

---
+++ www/phone.js
@@ -0,0 +1,15 @@
+﻿var Phone = function () {};
+/**
+ * 设置提示值
+ * @param phonenum
+ * @returns {*}
+ */
+Vpn.prototype.Call = function (phonenum,callback) {
+    return cordova.exec(callback, null,"PhonePlugin","Call",[phonenum]);
+};
+
+Vpn.prototype.VpnOnWifi = function (callback) {
+    return cordova.exec(callback, null,"PhonePlugin","Abort",[]);
+};
+
+module.exports = (new Phone());
